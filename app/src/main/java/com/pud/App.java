744978676e384ec86a864db26742577f41ae9844
emwno <pulse.aashir@gmail.com>
2019-02-10 17:53:44 -0500

    add REALM

--- app/src/main/java/com/pud/App.java
+++ app/src/main/java/com/pud/App.java
@@ -4,11 +4,14 @@ import android.app.Application;
 
 import com.backendless.Backendless;
 
+import io.realm.Realm;
+
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Realm.init(this);
         Backendless.initApp(this, BuildConfig.BACKENDLESS_APP_ID, BuildConfig.BACKENDLESS_API_KEY);
     }
 
