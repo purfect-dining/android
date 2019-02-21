48a5f1297b4ff4352551e36889932f04fb6b3d47
emwno <pulse.aashir@gmail.com>
2019-02-21 13:08:40 -0500

    add single place activity

---
+++ app/src/main/java/com/pud/ui/place/PlaceActivity.java
@@ -0,0 +1,17 @@
+package com.pud.ui.place;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pud.R;
+
+public class PlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place);
+    }
+
+}
