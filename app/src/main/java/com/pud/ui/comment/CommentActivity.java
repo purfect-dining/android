fb7bb969243da8667fc4281d8adade04c5cae332
emwno <pulse.aashir@gmail.com>
2019-03-01 18:03:45 -0500

    fix 2: migrate comment new/edit to new activity from place activity

---
+++ app/src/main/java/com/pud/ui/comment/CommentActivity.java
@@ -0,0 +1,178 @@
+package com.pud.ui.comment;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.pud.R;
+import com.pud.model.Comment;
+import com.pud.model.DiningTiming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @BindView(R.id.comment_toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.comment_input_text)
+    EditText mCommentEditText;
+
+    private String mDiningTimingID;
+    private String mCommentID;
+    private String mCommentText;
+    private String mCommentRating;
+    private boolean mEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+        mDiningTimingID = getIntent().getStringExtra("comment_dining_id");
+        mEdit = getIntent().getBooleanExtra("comment_edit", false);
+
+        if (mEdit) {
+            mCommentID = getIntent().getStringExtra("comment_id");
+            mCommentText = getIntent().getStringExtra("comment_text");
+            mCommentRating = getIntent().getStringExtra("comment_rating");
+            mToolbar.setTitle(getString(R.string.comment_edit_title));
+            mCommentEditText.setText(mCommentText);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        if (Backendless.UserService.CurrentUser() != null) {
+            getMenuInflater().inflate(R.menu.place_toolbar, menu);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.comment_menu_post:
+                if (mEdit) {
+                    editComment();
+                } else {
+                    addComment();
+                }
+                break;
+        }
+        return true;
+    }
+
+    public void addComment() {
+        Backendless.Data.of(DiningTiming.class).findById(mDiningTimingID, new AsyncCallback<DiningTiming>() {
+            @Override
+            public void handleResponse(DiningTiming diningTiming) {
+                Comment comment = new Comment();
+
+                String comText = mCommentEditText.getText().toString();
+                comment.setText(comText);
+                comment.setRating("4");
+
+                Backendless.Data.of(Comment.class).save(comment, new AsyncCallback<Comment>() {
+                    @Override
+                    public void handleResponse(Comment response) {
+                        List<BackendlessUser> ut = new ArrayList<>();
+                        ut.add(Backendless.UserService.CurrentUser());
+                        Backendless.Data.of(Comment.class).setRelation(comment, "byUser", ut, new AsyncCallback<Integer>() {
+                            @Override
+                            public void handleResponse(Integer response) {
+
+                            }
+
+                            @Override
+                            public void handleFault(BackendlessFault fault) {
+                                Toast.makeText(CommentActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        List<Comment> ct = new ArrayList<>();
+                        ct.add(comment);
+                        Backendless.Data.of(DiningTiming.class).addRelation(diningTiming, "comments", ct, new AsyncCallback<Integer>() {
+                            @Override
+                            public void handleResponse(Integer response) {
+
+                            }
+
+                            @Override
+                            public void handleFault(BackendlessFault fault) {
+                                Toast.makeText(CommentActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        List<DiningTiming> dt = new ArrayList<>();
+                        dt.add(diningTiming);
+                        Backendless.Data.of(Comment.class).setRelation(comment, "ofDiningTiming", dt, new AsyncCallback<Integer>() {
+                            @Override
+                            public void handleResponse(Integer response) {
+                                Toast.makeText(CommentActivity.this, "Comment Posted", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+
+                            @Override
+                            public void handleFault(BackendlessFault fault) {
+                                Toast.makeText(CommentActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+                        Toast.makeText(CommentActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+                Toast.makeText(CommentActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void editComment() {
+        Comment comment = new Comment();
+        comment.setObjectId(mCommentID);
+        comment.setText(mCommentEditText.getText().toString().trim());
+        comment.setRating(mCommentRating);
+
+        Backendless.Data.save(comment, new AsyncCallback<Comment>() {
+            @Override
+            public void handleResponse(Comment response) {
+                Toast.makeText(CommentActivity.this, "Comment Updated", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+                Toast.makeText(CommentActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/pud/ui/place/PlaceActivity.java
+++ app/src/main/java/com/pud/ui/place/PlaceActivity.java
@@ -1,26 +1,20 @@
 package com.pud.ui.place;
 
-import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.backendless.Backendless;
-import com.backendless.BackendlessUser;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
 import com.pud.R;
 import com.pud.listener.RecyclerItemClickListener;
 import com.pud.model.Comment;
-import com.pud.model.DiningTiming;
 import com.pud.model.Place;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.pud.ui.comment.CommentActivity;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -42,7 +36,6 @@ public class PlaceActivity extends AppCompatActivity implements PlaceContract.Vi
     CommentAdapter mAdapter;
     private PlacePresenter mPresenter;
     private Place mPlace;
-    private List<Comment> ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +65,8 @@ public class PlaceActivity extends AppCompatActivity implements PlaceContract.Vi
                 onBackPressed();
                 break;
             case R.id.add_comment:
-                showAddItemDialog(this);
+                Intent intent = new Intent(this, CommentActivity.class);
+                startActivityForResult(intent, 777);
                 break;
         }
 
@@ -82,7 +76,6 @@ public class PlaceActivity extends AppCompatActivity implements PlaceContract.Vi
     @Override
     public void onPlaceLoaded(Place place) {
         this.mPlace = place;
-        ll = place.getDiningTimings().get(0).getComments();
         getSupportActionBar().setTitle(place.getName() + " " + place.getDiningTimings().get(0).getComments().size());
         mAdapter = new CommentAdapter(this, place.getDiningTimings().get(0).getComments());
         mList.setLayoutManager(new LinearLayoutManager(this));
@@ -105,167 +98,32 @@ public class PlaceActivity extends AppCompatActivity implements PlaceContract.Vi
         return false;
     }
 
-    private void showAddItemDialog(Context c) {
-        final EditText taskEditText = new EditText(c);
-        AlertDialog dialog = new AlertDialog.Builder(c)
+    @Override
+    public void onItemClick(View view, int position) {
+        AlertDialog dialog = new AlertDialog.Builder(PlaceActivity.this)
                 .setTitle("Ford Dining Court")
-                .setMessage("Enter comment")
-                .setView(taskEditText)
-                .setPositiveButton("Post", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                .setMessage("Are you sure you want to delete?")
+                .setPositiveButton("Yes", (dialog2, which) -> {
+                    Comment comment = mAdapter.getList().get(position);
+                    mAdapter.getList().remove(position);
 
-                        Backendless.Data.of(DiningTiming.class).findById("47373B01-FF70-D06F-FF3A-4724C499FF00", new AsyncCallback<DiningTiming>() {
-                            @Override
-                            public void handleResponse(DiningTiming diningTiming) {
-                                Comment comment = new Comment();
+                    Backendless.Data.of(Comment.class).remove(comment, new AsyncCallback<Long>() {
+                        @Override
+                        public void handleResponse(Long response) {
+                            mAdapter.notifyDataSetChanged();
 
-                                String comText = taskEditText.getText().toString();
-                                comment.setText(comText);
-                                comment.setRating("4");
+                            Toast.makeText(PlaceActivity.this, "Comment Deleted", Toast.LENGTH_SHORT).show();
+                        }
 
-                                Backendless.Data.of(Comment.class).save(comment, new AsyncCallback<Comment>() {
-                                    @Override
-                                    public void handleResponse(Comment response) {
-                                        List<BackendlessUser> ut = new ArrayList<>();
-                                        ut.add(Backendless.UserService.CurrentUser());
-                                        Backendless.Data.of(Comment.class).setRelation(comment, "byUser", ut, new AsyncCallback<Integer>() {
-                                            @Override
-                                            public void handleResponse(Integer response) {
-                                                Toast.makeText(PlaceActivity.this, "Comment Posted", Toast.LENGTH_SHORT).show();
-                                            }
+                        @Override
+                        public void handleFault(BackendlessFault fault) {
 
-                                            @Override
-                                            public void handleFault(BackendlessFault fault) {
-
-                                            }
-                                        });
-
-                                        List<Comment> ct = new ArrayList<>();
-                                        ct.add(comment);
-                                        Backendless.Data.of(DiningTiming.class).addRelation(diningTiming, "comments", ct, new AsyncCallback<Integer>() {
-                                            @Override
-                                            public void handleResponse(Integer response) {
-
-                                            }
-
-                                            @Override
-                                            public void handleFault(BackendlessFault fault) {
-                                                Toast.makeText(PlaceActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
-                                            }
-                                        });
-
-                                        List<DiningTiming> dt = new ArrayList<>();
-                                        dt.add(diningTiming);
-                                        Backendless.Data.of(Comment.class).setRelation(comment, "ofDiningTiming", dt, new AsyncCallback<Integer>() {
-                                            @Override
-                                            public void handleResponse(Integer response) {
-                                                mPresenter.getPlace(objectId);
-                                            }
-
-                                            @Override
-                                            public void handleFault(BackendlessFault fault) {
-                                                Toast.makeText(PlaceActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
-                                            }
-                                        });
-                                    }
-
-                                    @Override
-                                    public void handleFault(BackendlessFault fault) {
-                                        Toast.makeText(PlaceActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                            }
-
-                            @Override
-                            public void handleFault(BackendlessFault fault) {
-                                Toast.makeText(PlaceActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
+                        }
+                    });
                 })
                 .setNegativeButton("Cancel", null)
                 .create();
         dialog.show();
     }
 
-    @Override
-    public void onItemClick(View view, int position) {
-        AlertDialog dialog = new AlertDialog.Builder(this)
-                .setTitle("Ford Dining Court")
-                .setMessage("Edit comment")
-                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        AlertDialog dialog2 = new AlertDialog.Builder(PlaceActivity.this)
-                                .setTitle("Ford Dining Court")
-                                .setMessage("Are you sure you want to delete?")
-                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Comment comment = ll.get(position);
-                                        ll.remove(position);
-
-                                        Backendless.Data.of(Comment.class).remove(comment, new AsyncCallback<Long>() {
-                                            @Override
-                                            public void handleResponse(Long response) {
-                                                mAdapter.notifyDataSetChanged();
-
-                                                Toast.makeText(PlaceActivity.this, "Comment Deleted", Toast.LENGTH_SHORT).show();
-                                            }
-
-                                            @Override
-                                            public void handleFault(BackendlessFault fault) {
-
-                                            }
-                                        });
-                                    }
-
-                                })
-                                .setNegativeButton("Cancel", null)
-                                .create();
-                        dialog2.show();
-                    }
-
-                })
-                .setNegativeButton("Edit", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        final EditText taskEditText = new EditText(PlaceActivity.this);
-                        taskEditText.setText(ll.get(position).getText());
-                        AlertDialog dialog1 = new AlertDialog.Builder(PlaceActivity.this)
-                                .setTitle("Ford Dining Court")
-                                .setMessage("Edit comment")
-                                .setView(taskEditText)
-                                .setPositiveButton("Update", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        ll.get(position).setText(taskEditText.getText().toString());
-                                        Comment comment = ll.get(position);
-
-                                        Backendless.Data.save(comment, new AsyncCallback<Comment>() {
-                                            @Override
-                                            public void handleResponse(Comment response) {
-                                                mAdapter.notifyDataSetChanged();
-                                                Toast.makeText(PlaceActivity.this, "Comment Updated", Toast.LENGTH_SHORT).show();
-
-
-                                            }
-
-                                            @Override
-                                            public void handleFault(BackendlessFault fault) {
-                                                Toast.makeText(PlaceActivity.this, fault.getMessage(), Toast.LENGTH_SHORT).show();
-                                            }
-                                        });
-                                    }
-
-                                })
-                                .setNegativeButton("Cancel", null)
-                                .create();
-                        dialog1.show();
-                    }
-                })
-                .create();
-        dialog.show();
-    }
 }
