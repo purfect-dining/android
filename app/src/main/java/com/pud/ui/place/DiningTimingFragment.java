f0d1e04f792f784f9e89093c0600a5a2c812817e
emwno <pulse.aashir@gmail.com>
2019-03-24 21:52:44 -0400

    display menu for each dining court

--- app/src/main/java/com/pud/ui/place/DiningTimingFragment.java
+++ app/src/main/java/com/pud/ui/place/DiningTimingFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.pud.R;
 import com.pud.model.DiningTiming;
+import com.pud.model.MenuItem;
+import com.pud.model.MenuSection;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -25,7 +28,24 @@ public class DiningTimingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_diningtiming, container, false);
-        ((TextView) view.findViewById(R.id.diningtiming_text)).setText(((DiningTiming) getArguments().getParcelable("diningTiming")).getDiningType().getName());
+        DiningTiming diningTiming = getArguments().getParcelable("diningTiming");
+        String s = diningTiming.getDiningType().getName() + "\n" +
+                diningTiming.getFrom().toString().substring(11, 16) + " - " +
+                diningTiming.getTo().toString().substring(11, 16);
+        ((TextView) view.findViewById(R.id.diningtiming_text)).setText(s);
+        LinearLayout layout = view.findViewById(R.id.menu);
+        for (MenuSection section : diningTiming.getMenuSections()) {
+            TextView ts = new TextView(getContext());
+            ts.setText(section.getName());
+            ts.setTextSize(20);
+            ts.setPadding(0, 10, 0, 0);
+            layout.addView(ts);
+            for (MenuItem item : section.getMenuItems()) {
+                TextView ti = new TextView(getContext());
+                ti.setText(item.getName());
+                layout.addView(ti);
+            }
+        }
         return view;
     }
 }
